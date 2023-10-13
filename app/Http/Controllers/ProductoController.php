ba789d2e21b0e8b49f48ba086942e6d34bb4f831
cristian <cchura.cpc@gmail.com>
2023-10-12 21:10:45 -0400

    listado y guardar producto api

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Producto;
 use Illuminate\Http\Request;
 
 class ProductoController extends Controller
@@ -9,9 +10,26 @@ class ProductoController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        // /api/producto?q=&page=1
+        $q = isset($request->q)?$request->q:null;
+        if($q){
+
+            $productos = Producto::where('nombre', 'like', '%'.$q.'%')
+                                    ->orWhere('precio', 'like', '%'.$q.'%')
+                                    ->orderBy('id', 'desc')
+                                    ->with('categoria')
+                                    ->paginate(2);
+
+        }else{
+            $productos = Producto::orderBy('id', 'desc')
+                                    ->with('categoria')
+                                    ->paginate(2);
+        }
+
+        return response()->json($productos);
+
     }
 
     /**
@@ -19,7 +37,22 @@ class ProductoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // validamos
+        $request->validate([
+            "nombre" => "required",
+            "categoria_id" => "required",
+            "precio" => "required"
+        ]);
+        
+        $producto = new Producto();
+        $producto->nombre = $request->nombre;
+        $producto->precio = $request->precio;
+        $producto->stock = $request->stock;
+        $producto->descripcion = $request->descripcion;
+        $producto->categoria_id = $request->categoria_id;
+        $producto->save();
+
+        return response()->json(["message" => "Producto Registrado"], 201);
     }
 
     /**
